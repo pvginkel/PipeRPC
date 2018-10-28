1b34a94a129088935a9d9952fca7e2c7a73372b5
Pieter van Ginkel <pvginkel@gmail.com>
2018-10-28 10:52:33 +0100

    Added a sample client and server.

---
+++ SampleClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PipeRpc;
+
+namespace SampleClient
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Start the RPC client with the handle to the RPC server
+            // and an instance of the service to host.
+
+            using (var client = new PipeRpcClient(PipeRpcHandle.FromString(args[0]), new Service()))
+            {
+                client.Run();
+            }
+        }
+    }
+}
---
+++ SampleServer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PipeRpc;
+
+namespace SampleServer
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var server = new PipeRpcServer())
+            {
+                // Start the server with the location and arguments to the RPC client.
+
+                server.Start(new ClientStartInfo(
+                    typeof(SampleClient.Program).Assembly.Location,
+                    null,
+                    server.Handle.ToString()
+                ));
+
+                // Invoke a method on the client service.
+
+                int result = server.Invoke<int>("Add", 1, 2);
+
+                // Write the results.
+
+                Console.WriteLine($"1 + 2 = {result}");
+            }
+        }
+    }
+}
